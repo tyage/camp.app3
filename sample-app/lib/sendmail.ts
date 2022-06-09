22e7f6fe027b97cfd5a8e4a16963ae79a922e772
tyage <namatyage@gmail.com>
2022-06-09 11:49:10 +0900

    replace mailgun-js to mailgun.js

--- sample-app/lib/sendmail.ts
+++ sample-app/lib/sendmail.ts
@@ -1,16 +1,21 @@
-import mailgun from 'mailgun-js'
+import Mailgun from 'mailgun.js'
+import formData from 'form-data'
+import { MessagesSendResult } from 'mailgun.js/interfaces/Messages'
 
-export function sendMail(to: string, subject: string, text: string) {
+const mailgun = new Mailgun(formData)
+
+export async function sendMail(to: string, subject: string, text: string): Promise<MessagesSendResult> {
   const domain = process.env.MAILGUN_DOMAIN || ""
   const apiKey = process.env.MAILGUN_API_KEY || ""
-  const mg = mailgun({ apiKey, domain })
+  const mg = mailgun.client({
+    username: 'api',
+    key: apiKey
+  })
 
-  mg.messages().send({
+  return await mg.messages.create(domain, {
     from: 'Yurucamp app <me@samples.mailgun.org>',
     to,
     subject,
     text,
-  }, function (error, body) {
-    console.log(body)
   })
 }
