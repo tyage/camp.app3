a466bf1d4df03025708ad0170ff66acdec2b175a
tyage <namatyage@gmail.com>
2022-06-13 10:59:38 +0900

    use default charset (utf8_general_mb4)

--- sample-app/lib/db.ts
+++ sample-app/lib/db.ts
@@ -10,7 +10,7 @@ export async function getConnection(): Promise<mysql.Connection> {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_DATABASE
   })
 
   cachedConnection = connection
