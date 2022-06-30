2cb4044ffc9b560dedba3163aa143d072e3cb28c
tyage <namatyage@gmail.com>
2022-06-30 14:49:43 +0900

    use mailjet

--- sample-app/lib/sendmail.ts
+++ sample-app/lib/sendmail.ts
@@ -2,8 +2,30 @@ import Mailgun from 'mailgun.js'
 import formData from 'form-data'
 import { MessagesSendResult } from 'mailgun.js/interfaces/Messages'
 import sendgrid, { ClientResponse } from '@sendgrid/mail'
+import Mailjet from 'node-mailjet'
+import { IAPIResponse } from 'node-mailjet/declarations/types/api/Response'
+import { TRequestData } from 'node-mailjet/declarations/request/IRequest'
 
-export async function sendMailWithSendgrid(to: string, subject: string, text: string): Promise<[ClientResponse, {}]> {
+const mailjet = new Mailjet({
+  apiKey: process.env.MJ_APIKEY_PUBLIC,
+  apiSecret: process.env.MJ_APIKEY_PRIVATE
+})
+export async function sendMailViaMailjet(to: string, subject: string, text: string): Promise<IAPIResponse<TRequestData>> {
+  return await mailjet
+    .post('send', { version: 'v3.1' })
+    .request({
+      Messages: [
+        {
+          From: { Email: 'namatyage@gmail.com' },
+          To: [{ Email: to }],
+          Subject: subject,
+          TextPart: text
+        }
+      ]
+    })
+}
+
+export async function sendMailViaSendgrid(to: string, subject: string, text: string): Promise<[ClientResponse, {}]> {
   const API_KEY = process.env.SENDGRID_API_KEY || ""
   sendgrid.setApiKey(API_KEY)
 
@@ -17,8 +39,7 @@ export async function sendMailWithSendgrid(to: string, subject: string, text: st
 }
 
 const mailgun = new Mailgun(formData)
-
-export async function sendMailWithMailgun(to: string, subject: string, text: string): Promise<MessagesSendResult> {
+export async function sendMailViaMailgun(to: string, subject: string, text: string): Promise<MessagesSendResult> {
   const domain = process.env.MAILGUN_DOMAIN || ""
   const apiKey = process.env.MAILGUN_API_KEY || ""
   const mg = mailgun.client({
